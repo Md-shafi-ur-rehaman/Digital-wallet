04ffe730d614aa46fe487e2c290780abe25d1917
Md-shafi-ur-rehaman <mohammadshafiq.rahaman@gamil.com>
2024-12-15 17:33:13 +0530

    additional

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose');
-const password = encodeURIComponent("J3@E#FHy3bYZxe.");
-const MONGO_URL = `mongodb+srv://Shafi:${password}@digital-wallet.nfaon.mongodb.net/?retryWrites=true&w=majority&appName=Digital-wallet`;
+
+
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(MONGO_URL);
+    await mongoose.connect(process.env.MONGO_URL);
     console.log("database is connected successfully!");
   } catch (err) {
     console.log(err);
